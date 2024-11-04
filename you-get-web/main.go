d5667214eaa63c65291a1f3cf3b9c493e892df7d
youyg <youyg8848@outlook.com>
2024-11-04 18:00:38 +0800

    add CORS config

--- you-get-web/main.go
+++ you-get-web/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os/exec"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/thanhpk/randstr"
 )
@@ -19,17 +20,28 @@ func setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},  // 允许的来源，可以是多个
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"}, // 允许的请求方法
+		AllowHeaders:     []string{"Origin", "Content-Type"}, // 允许的请求头
+		ExposeHeaders:    []string{"Content-Length"},         // 允许浏览器访问的响应头
+		AllowCredentials: true,                               // 是否允许发送 Cookie
+		MaxAge:           12 * 3600,                          // 预检请求的缓存时间
+	}))
 
 	r.POST("/download", func(c *gin.Context) {
 		iflag := randstr.String(16)
 		var req DownloadReq
-		if err := c.ShouldBind(&req); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		req.Url = c.PostForm("url")
+		req.Proxy = c.PostForm("proxy")
+		if req.Url == "" {
+			c.JSON(http.StatusNoContent, gin.H{"error": "url is empty"})
 			return
 		}
 
 		file, err1 := c.FormFile("cookies")
 		var cookiesName string
+		log.Println(err1)
 		if err1 == nil {
 			log.Println(file.Filename)
 			cookiesName = fmt.Sprintf("/Temp/cookies/%s.txt", iflag)
@@ -38,10 +50,10 @@ func setupRouter() *gin.Engine {
 
 		var args []string
 		args = append(args, req.Url, "-o", "/Temp/you-get/download")
-		if req.Proxy!= "" { 
+		if req.Proxy != "" {
 			args = append(args, "-x", req.Proxy)
 		}
-		if cookiesName!= "" {
+		if cookiesName != "" {
 			args = append(args, "-c", cookiesName)
 		}
 
@@ -53,6 +65,12 @@ func setupRouter() *gin.Engine {
 		}
 
 		c.JSON(http.StatusOK, gin.H{"status": req.Url + iflag})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
 	})
 
 	return r
